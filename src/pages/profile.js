eb254f496528503b82ba08c443133de0027f2b90
Enkai <enkaihuang2021@gmail.com>
2023-03-26 11:58:43 +0800

    fix userlist bug

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,23 +1,26 @@
 import React, { useEffect, useState } from "react";
 import avatar from "./avatar.png";
 import "./profile.css";
-import { db } from "../backend/firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { db, auth } from "../backend/firebase";
+import { collection, doc, getDoc } from "firebase/firestore";
 
 function Profile() {
-  const [userList, setUserList] = useState([]);
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
     const userInfoCollection = collection(db, "users");
+    const currentUser = auth.currentUser;
+
     const getUserInfo = async () => {
       try {
-        const data = await getDocs(userInfoCollection);
-        const filteredData = await data.docs.map((doc) => ({
-          ...doc.data(),
-          id: doc.id,
-        }));
-        setUserList(filteredData);
-        console.log(filteredData);
+        const userDoc = doc(userInfoCollection, currentUser.uid);
+        const userData = await getDoc(userDoc);
+
+        if (userData.exists()) {
+          setUserData(userData.data());
+        } else {
+          console.log("No such user document!");
+        }
       } catch (error) {
         console.log(error);
       }
@@ -25,28 +28,58 @@ function Profile() {
     getUserInfo();
   }, []);
 
+  if (!userData) {
+    return <p>Loading...</p>;
+  }
+
+  const { name, email, score } = userData;
+
   return (
     <nav className="menu" tabIndex="0">
       <div className="smartphone-menu-trigger"></div>
       <header className="avatar">
         <img src={avatar} alt="Avatar" />
         <div className="profile__info">
-              {userList.map((user) => (
-                <div className="profile__info__item" key={user.id}>
-                  <h3 className="username">{user.name}</h3>
-                  <p className="usermail"><i class="fa fa-envelope" aria-hidden="true"></i> {user.email}</p>
-                  <p className="userscore">{user.score}</p>
-                  {/* <i class="fa fa-trophy" aria-hidden="true"></i> */}
-                </div>
-              ))}
-            </div>
+          <div className="profile__info__item">
+            <h3 className="username">{name}</h3>
+            <p className="usermail">
+              <i class="fa fa-envelope" aria-hidden="true"></i> {email}
+            </p>
+            <p className="userscore">{score}</p>
+          </div>
+        </div>
       </header>
       <ul>
-        <li tabIndex="0" className="icon-contest"><i class="fa fa-tasks" aria-hidden="true"></i><span> Contest</span></li>
-        <li tabIndex="0" className="icon-ranking"><i class="fa fa-bar-chart" aria-hidden="true"></i> <span>Ranking</span></li>
-        <li tabIndex="0" className="icon-forum" style={{color: "#808080", backgroundColor:"	#696969"}}><i class="fa fa-comments-o" aria-hidden="true"></i> <span>Forum</span></li>
-        <li tabIndex="0" className="icon-users" style={{color: "#808080", backgroundColor:"	#696969"}}><i class="fa fa-user" aria-hidden="true"></i> <span>Profile</span></li>
-        <li tabIndex="0" className="icon-settings" style={{color: "#808080", backgroundColor:"	#696969"}}><i class="fa fa-cog" aria-hidden="true"></i> <span>Settings</span></li>
+        <li tabIndex="0" className="icon-contest">
+          <i class="fa fa-tasks" aria-hidden="true"></i>
+          <span> Contest</span>
+        </li>
+        <li tabIndex="0" className="icon-ranking">
+          <i class="fa fa-bar-chart" aria-hidden="true"></i>{" "}
+          <span>Ranking</span>
+        </li>
+        <li
+          tabIndex="0"
+          className="icon-forum"
+          style={{ color: "#808080", backgroundColor: "#696969" }}
+        >
+          <i class="fa fa-comments-o" aria-hidden="true"></i>{" "}
+          <span>Forum</span>
+        </li>
+        <li
+          tabIndex="0"
+          className="icon-users"
+          style={{ color: "#808080", backgroundColor: "#696969" }}
+        >
+          <i class="fa fa-user" aria-hidden="true"></i> <span>Profile</span>
+        </li>
+        <li
+          tabIndex="0"
+          className="icon-settings"
+          style={{ color: "#808080", backgroundColor: "#696969" }}
+        >
+          <i class="fa fa-cog" aria-hidden="true"></i> <span>Settings</span>
+        </li>
       </ul>
     </nav>
   );
