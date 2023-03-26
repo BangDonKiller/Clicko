c7e832bf75504e3bdabfbe09892a10ca65edbb9b
Enkai <enkaihuang2021@gmail.com>
2023-03-26 13:22:34 +0800

    add contest page

---
+++ src/pages/contest.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./contest.css";
+
+function Contest(){
+  return (
+    <div>
+      <h1>Welcome to the Contest Page!</h1>
+      <p>Here you can participate in exciting contests and win prizes.</p>
+    </div>
+  )
+};
+
+export default Contest;
