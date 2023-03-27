9da7b1b902888ecf9712a5042f62fe379ddf506f
Enkai <enkaihuang2021@gmail.com>
2023-03-27 15:15:26 +0800

    contest UI

--- src/pages/contest.js
+++ src/pages/contest.js
@@ -1,13 +1,158 @@
 import React from "react";
 import "./contest.css";
 
-function Contest(){
+function Contest() {
   return (
     <div className="contest_bg">
-      <h1>Welcome to the Contest Page!</h1>
-      <p>Here you can participate in exciting contests and win prizes.111</p>
+      <section className="notifications">
+        <div class="coffee-card">
+          <span class="title">&#9749; Clicko Notice</span>
+          <p class="description">
+            This website isn't complete yet. <br />
+            There'll be a new function activated everytime 10 new users join
+            this community.
+          </p>
+        </div>
+        <div class="coffee-card">
+          <span class="title">&#x1F4B5; Donation Notice</span>
+          <p class="description">
+            Since this website isn't complete, you can donate to us. If the
+            donation is more than 100$, you'll get a free premium and a new
+            function will be enabled.
+          </p>
+        </div>
+        <div class="coffee-card">
+          <span class="title">&#x1F41E; Bug Report Notice</span>
+          <p class="description">
+            If you find any bugs, please report them to us (Coffee club leader
+            is one of the members).
+            <br />
+            We'll fix them as soon as possible.
+            <br />
+          </p>
+        </div>
+      </section>
+      <section className="contest_board">
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+        <div class="h-divider">
+          <div class="shadow"></div>
+        </div>
+        <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Start: 2021-01-01 00:00:00</div>
+          <div className="contest_join_code">Join Code: 123456789</div>
+        </div>
+      </section>
     </div>
-  )
-};
+  );
+}
 
 export default Contest;
