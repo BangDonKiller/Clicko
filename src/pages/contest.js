c8f08f9005f2e9d5adacae59c1d93558a9e98832
Enkai <enkaihuang2021@gmail.com>
2023-03-27 19:12:34 +0800

    add create clicko button

--- src/pages/contest.js
+++ src/pages/contest.js
@@ -5,26 +5,27 @@ function Contest() {
   return (
     <div className="contest_bg">
       <section className="notifications">
-        <div class="coffee-card">
-          <span class="title">&#9749; Clicko Notice</span>
-          <p class="description">
+        <div className="coffee-card">
+          <span className="title">&#9749; Clicko Notice</span>
+          <p className="description">
             We are still in the process of building this website. Every time 20
             new members join, we will add a new feature to it.
           </p>
         </div>
-        <div class="coffee-card">
-          <span class="title">&#x1F4B5; Donation Notice</span>
-          <p class="description">
+        <div className="coffee-card">
+          <span className="title">&#x1F4B5; Donation Notice</span>
+          <p className="description">
             This is a work in progress, you can donate to keep it going. If you
             donate more than 100$, you'll get a free premium membership.
           </p>
         </div>
-        <div class="coffee-card">
-          <span class="title">&#x1F41E; Bug Report Notice</span>
-          <p class="description">
+        <div className="coffee-card">
+          <span className="title">&#x1F41E; Bug Report Notice</span>
+          <p className="description">
             If you find any bugs, please report them to us. We'll fix them as
             soon as possible. <br />
-            Find us on <a href="https://www.facebook.com/NCUcafeclub">Facebook</a>.
+            Find us on{" "}
+            <a href="https://www.facebook.com/NCUcafeclub">Facebook</a>.
             <br />
           </p>
         </div>
@@ -35,209 +36,236 @@ function Contest() {
           <div className="contest_time">Pending...</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
-          {/* <div className="contest_join_code">Join Code: 123456789</div> */}
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
         </div>
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">Complete</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
         </div>
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">In Progress</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
         </div>
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">Pending...</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
         </div>
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">Pending...</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
+        </div>
+        {/* <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Pending...</div>
+          <input
+            type="text"
+            autoComplete="off"
+            name="text"
+            className="input"
+            placeholder="Join Code"
+          ></input>
+        </div>
+        <div className="h-divider">
+          <div className="shadow"></div>
         </div>
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">Pending...</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
         </div>
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">Pending...</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
         </div>
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">Pending...</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
         </div>
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">Pending...</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
         </div>
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">Pending...</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
         </div>
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">Pending...</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
         </div>
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">Pending...</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div className="h-divider">
+          <div className="shadow"></div>
+        </div> */}
+        {/* <div className="contest_item">
+          <div className="contest_title">Contest001</div>
+          <div className="contest_time">Pending...</div>
+          <input
+            type="text"
+            autoComplete="off"
+            name="text"
+            className="input"
+            placeholder="Join Code"
+          ></input>
         </div>
+        <div className="h-divider">
+          <div className="shadow"></div>
+        </div> */}
         <div className="contest_item">
           <div className="contest_title">Contest001</div>
           <div className="contest_time">Pending...</div>
           <input
             type="text"
-            autocomplete="off"
+            autoComplete="off"
             name="text"
-            class="input"
+            className="input"
             placeholder="Join Code"
           ></input>
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
+        <div
+          style={{
+            height: "20px",
+          }}
+        />
+      </section>
+      <section className="add_contest">
+        <div className="create_contest">
+          <span className="title">Create your Clicko</span>
+          <div className="create_description">
+            Create a Clicko and invite your friends to compete with you.
+            <div className="light-loader">
+              <div className="loader"></div>
+            </div>
+          </div>
         </div>
-        <div className="contest_item">
-          <div className="contest_title">Contest001</div>
-          <div className="contest_time">Pending...</div>
+        <div className="create_clicko">
           <input
             type="text"
-            autocomplete="off"
-            name="text"
-            class="input"
-            placeholder="Join Code"
-          ></input>
+            className="clicko_name"
+            id="clicko_name"
+            name="clicko_name"
+            placeholder="Clicko Name"
+            autoComplete="off"
+          />
+          <input className="button--submit" value="Create" type="submit" />
         </div>
-        <div class="h-divider">
-          <div class="shadow"></div>
-        </div>
-        <div className="contest_item">
-          <div className="contest_title">Contest001</div>
-          <div className="contest_time">Pending...</div>
-          <input
-            type="text"
-            autocomplete="off"
-            name="text"
-            class="input"
-            placeholder="Join Code"
-          ></input>
-        </div>
+        <div className="create_footer">Time left: 5/5</div>
       </section>
     </div>
   );
