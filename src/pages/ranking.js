c032cf465dca80b9fe769c15fac03825b1cf3d8c
Enkai <enkaihuang2021@gmail.com>
2023-03-28 15:25:32 +0800

    fix key bug in list

--- src/pages/ranking.js
+++ src/pages/ranking.js
@@ -13,7 +13,9 @@ function Ranking() {
     const getUsers = async () => {
       const q = query(collection(db, "users"), orderBy("score", "desc"), limit(10));
       const querySnapshot = await getDocs(q);
-      const usersData = querySnapshot.docs.map((doc) => doc.data());
+      const usersData = querySnapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data() };
+      });
       setUsers(usersData);
     };
     getUsers();
@@ -47,88 +49,3 @@ function Ranking() {
 }
 
 export default Ranking;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function Ranking() {
-//   return (
-//     <div className="ranking_bg">
-//       <section className="top_three">
-//         <div className="first_place">
-//           <img src={avatar} alt="Avatar" className="avatar" />
-//           <div className="player_name">John Doe</div>
-//           <div className="player_score">- 1700 pts -</div>
-//         </div>
-//         <div className="second_place">
-//           <img src={avatar} alt="Avatar" className="avatar" />
-//           <div className="player_name">Tom Cruise</div>
-//           <div className="player_score">- 1600 pts -</div>
-//         </div>
-//         <div className="third_place">
-//           <img src={avatar} alt="Avatar" className="avatar" />
-//           <div className="player_name">Gerald Jinx</div>
-//           <div className="player_score">- 1500 pts -</div>
-//         </div>
-//       </section>
-//       <section className="ranking_board">
-//         <div className="user_ranking">
-//           <div className="ranking_num">4. </div>
-//           <div className="ranking_name">Jason</div>
-//           <div className="ranking_score">1400 pts</div>
-//         </div>
-//         <b class="hr anim"></b>
-//         <div className="user_ranking">
-//           <div className="ranking_num">5. </div>
-//           <div className="ranking_name">Emily</div>
-//           <div className="ranking_score">1300 pts</div>
-//         </div>
-//         <b class="hr anim"></b>
-//         <div className="user_ranking">
-//           <div className="ranking_num">6. </div>
-//           <div className="ranking_name">Jason</div>
-//           <div className="ranking_score">1400 pts</div>
-//         </div>
-//         <b class="hr anim"></b>
-//         <div className="user_ranking">
-//           <div className="ranking_num">7. </div>
-//           <div className="ranking_name">Emily</div>
-//           <div className="ranking_score">1300 pts</div>
-//         </div>
-//         <b class="hr anim"></b>
-//         <div className="user_ranking">
-//           <div className="ranking_num">8. </div>
-//           <div className="ranking_name">Jason</div>
-//           <div className="ranking_score">1400 pts</div>
-//         </div>
-//         <b class="hr anim"></b>
-//         <div className="user_ranking">
-//           <div className="ranking_num">9. </div>
-//           <div className="ranking_name">Emily</div>
-//           <div className="ranking_score">1300 pts</div>
-//         </div>
-//       </section>
-//     </div>
-//   );
-// }
-
-// export default Ranking;
